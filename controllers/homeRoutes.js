f7703fa1a5ac57b26cdcbd923388cfca8ca07ff4
Ermiyas B <124329320+Ermi-B@users.noreply.github.com>
2023-06-24 22:51:39 -0500

    added routes and basic frontend

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,5 +8,29 @@ router.get('/contact',(req,res)=>{
     res.render('contact',{})
 })
 
+router.get('/pricing',(req,res)=>{
+    res.render('pricing',{})
+})
+
+router.get('/blogs',(req,res)=>{
+    res.render('blogs',{})
+})
+
+router.get('/booking',(req,res)=>{
+    res.render('booking',{})
+})
+
+
+router.get('/about',(req,res)=>{
+    res.render('about')
+})
+
+router.get('/gallery',(req,res)=>{
+    res.render('gallery')
+})
+
+router.get('/success',(req,res)=>{
+    res.render('success')
+})
 
 module.exports = router;
---
+++ public/js/home.js
@@ -0,0 +1,43 @@
+const carousel = document.getElementById("carousel")
+const firstImg = carousel.querySelectorAll("img")[0];
+const lastImg = carousel.querySelectorAll("img")[-1]
+arrowIcons = document.querySelectorAll(".wrapper i")
+
+let firstImgWidth = firstImg.clientWidth +(firstImg.clientWidth*2); //adding margin value
+arrowIcons.forEach(icon=>{
+    const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
+    icon.addEventListener("click",()=>{
+       carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+       if (carousel.scrollLeft >= maxScrollLeft) {
+        carousel.scrollLeft = 0; // Reset to the beginning if reached the end
+      } else {
+        carousel.scrollLeft += firstImgWidth;
+      }
+
+    })
+})
+
+carousel.addEventListener("mouseenter", () => {
+    clearInterval(intervalId); // Stop the interval when the mouse enters the carousel
+  });
+  
+  carousel.addEventListener("mouseleave", () => {
+    intervalId = setInterval(() => {
+      carousel.scrollLeft += firstImgWidth;
+    }, 7000); // Start the interval when the mouse leaves the carousel
+  });
+  
+//   // Start the interval initially
+//   intervalId = setInterval(() => {
+//     carousel.scrollLeft += firstImgWidth;
+//   }, 7000);
+// Start the interval initially
+intervalId = setInterval(() => {
+    const scrollLeft = carousel.scrollLeft;
+    const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
+    if (scrollLeft >= maxScrollLeft) {
+      carousel.scrollLeft = 0; // Reset to the beginning if reached the end
+    } else {
+      carousel.scrollLeft += firstImgWidth;
+    }
+  }, 10000);
